90566892f5f771cf45bf1e48c699dfd9fe8e77ba
Hervé Tutuaku <herve.tutuaku@gmail.com>
2016-07-18 10:36:20 +0200

    [fix] refactor mail.js

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,174 +1,158 @@
 $(document).ready(function () {
-	/**
-	 * Full calendar
-	 */
-    $('#calendar').fullCalendar({
-    	header: {
-    		left: '',
-    		right: ''
-        },
-		dayClick: function(date, jsEvent, view) {
+  /**
+   * Full calendar
+   */
+  $('#calendar').fullCalendar({
+    header: {
+      left: '',
+      right: ''
+    },
+    dayClick: function(date, jsEvent, view) {
 
-		    // console.info('---');
-			// console.log('Clicked on: ' + date.format());
-		    // console.log('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-		    // console.log('Current view: ' + view.name);
+        // console.info('---');
+      // console.log('Clicked on: ' + date.format());
+        // console.log('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+        // console.log('Current view: ' + view.name);
 
-		    // FIXME: remove handler until we know how to manage event creation
-		    // $('.add-button').remove();
-		    // $('.fc-today').removeClass('fc-today');
-		    // $('.fc-state-highlight').removeClass('fc-state-highlight');
+        // FIXME: remove handler until we know how to manage event creation
+        // $('.add-button').remove();
+        // $('.fc-today').removeClass('fc-today');
+        // $('.fc-state-highlight').removeClass('fc-state-highlight');
 
-		    // // change the day's background color just for fun
-		    // $(this).addClass('fc-state-highlight fc-today');
-		    // $(this).append('<button class="add-button btn btn-default">+</button>');
+        // // change the day's background color just for fun
+        // $(this).addClass('fc-state-highlight fc-today');
+        // $(this).append('<button class="add-button btn btn-default">+</button>');
 
-		},
-		handleWindowResize: true,
-		windowResize: function(view) {
-        	
-    	}
-    });
-
-    var moment = $('#calendar').fullCalendar('getDate');
-    var currDayCol = moment.format('ddd');
-
-    $('.dataToday').html(moment.format('MMMM YYYY'));
-    
-    // FIXME: trigger on calendar render
-    $('th.fc-day-header.fc-widget-header.fc-' + currDayCol.toLowerCase()).addClass('current-day-column');
-
-    $('#calendar').fullCalendar('addEventSource', [
-        {
-            title  : 'event1',
-            start  : '2016-07-01'
-        },
-        {
-            title  : 'event1',
-            start  : '2016-07-01'
-        },
-        {
-            title  : 'event1',
-            start  : '2016-07-06'
-        },
-        {
-            title  : 'event1',
-            start  : '2016-07-06'
-        },
-        {
-            title  : 'event1',
-            start  : '2016-07-06'
-        },
-        {
-            title  : 'event1',
-            start  : '2016-07-06'
-        },
-        {
-            title  : 'event1',
-            start  : '2016-07-06'
-        },
-        {
-            title  : 'event1',
-            start  : '2016-07-06'
-        },
-        {
-            title  : 'event1',
-            start  : '2016-07-06'
-        },
-        {
-            title  : 'event2',
-            start  : '2016-07-01',
-            end    : '2016-07-01'
-        },
-        {
-            title  : 'event3',
-            start  : '2016-07-01T12:30:00',
-            allDay : false // will make the time show
-        }
-    ]);
-    
-    $('#prevButton').click(function(){
-    	$('#calendar').fullCalendar('prev');
-    });
-    $('#nextButton').click(function(){
-    	$('#calendar').fullCalendar('next');
-    });
-    $('#todayButton').click(function(){
-    	$('#calendar').fullCalendar('today');
-    });
-    $('#monthButton').click(function(){
-    	$('#calendar').fullCalendar( 'changeView', 'month' );
-    });
-    $('#weekButton').click(function(){
-    	$('#calendar').fullCalendar( 'changeView', 'basicWeek' );
-    });
-    $('#dayButton').click(function(){
-    	$('#calendar').fullCalendar( 'changeView', 'basicDay' );
-    });
-
-    /**
-     * Add Google Map
-     */
-    var geocoder;
-	var map;
-	var marker;
-
-	var map = document.getElementById('map-canvas');
-
-	// Lunch google map if the canvas exit
-	// so if we are on the right page
-	if (map) {
-		// Lancement de la construction de la carte google map
-		google.maps.event.addDomListener(window, 'load', initMap);
-
-		$('#datetimepickerStart').datetimepicker({
-		    locale: 'fr'
-		});
-		$('#datetimepickerFinish').datetimepicker({
-		    locale: 'fr'
-		});
-	}
-});
-
-// initialisation de la carte Google Map de départ
-function initMap() {
-  geocoder = new google.maps.Geocoder();
-  // Ici j'ai mis la latitude et longitude du vieux Port de Marseille pour centrer la carte de départ
-  var latlng = new google.maps.LatLng(48.856614, 2.3522219000000177);
-  var mapOptions = {
-    zoom      : 14,
-    center    : latlng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-  }
-  // map-canvas est le conteneur HTML de la carte Google Map
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-}
- 
-function findPlace() {
-  // Réinitialisation du marqueur		
-  if(marker !=null)
-	  marker.setMap(null);
-  // Récupération de l'adresse tapée dans le formulaire
-  var adresse = document.getElementById('maps').value;
-  geocoder.geocode( { 'address': adresse}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      // Récupération des coordonnées GPS du lieu tapé dans le formulaire
-      var strposition = results[0].geometry.location+"";
-      strposition=strposition.replace('(', '');
-      strposition=strposition.replace(')', '');
-      // Affichage des coordonnées dans le <span>
-      document.getElementById('text_latlng').innerHTML='Coordonnées : '+strposition;
-      //Récupération des coordonnées dans le formulaire
-      document.getElementById('latitude').value=results[0].geometry.location.lat();
-      document.getElementById('longitude').value=results[0].geometry.location.lng();
-      // Création du marqueur du lieu (épingle)
-      marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert('Adresse introuvable: ' + status);
-    }
+    },
+    handleWindowResize: true,
+    windowResize: function(view) {
+        
+    },
+    // events: [{
+    //   title:"My repeating event",
+    //   start: '10:00', // a start time (10am in this example)
+    //   // end: '14:00', // an end time (6pm in this example)
+    //   dow: [ 1, 4 ] // Repeat monday and thursday
+    // }],
+    events: 'json/getallevents'
   });
-}
+
+  currDayCol = "";
+  var moment = $('#calendar').fullCalendar('getDate');
+  if (moment.format) {
+    var currDayCol = moment.format('ddd');
+    $('.dataToday').html(moment.format('MMMM YYYY'));
+  }
+
+  // Add calendar button listeners
+  $('th.fc-day-header.fc-widget-header.fc-' + currDayCol.toLowerCase()).addClass('current-day-column');
+  
+  $('#prevButton').click(function(){
+    $('#calendar').fullCalendar('prev');
+  });
+  $('#nextButton').click(function(){
+    $('#calendar').fullCalendar('next');
+  });
+  $('#todayButton').click(function(){
+    $('#calendar').fullCalendar('today');
+  });
+  $('#monthButton').click(function(){
+    $('#calendar').fullCalendar( 'changeView', 'month' );
+  });
+  $('#weekButton').click(function(){
+    $('#calendar').fullCalendar( 'changeView', 'basicWeek' );
+  });
+  $('#dayButton').click(function(){
+    $('#calendar').fullCalendar( 'changeView', 'basicDay' );
+  });
+
+  // $('#calendar').fullCalendar('addEventSource', [
+  // ]);
+
+  /**
+   * Add Google Map
+   */
+  var geocoder;
+  var map;
+  var marker;
+
+  var map = document.getElementById('map-canvas');
+
+  // Lunch google map if the canvas exit
+  // so if we are on the right page
+  if (map) {
+    // Lancement de la construction de la carte google map
+    google.maps.event.addDomListener(window, 'load', initMap);
+
+    // $('#datetimepickerStart').datepicker({
+    //     locale: 'fr',
+    //     useCurrent: true,
+    // });
+    // $('#datetimepickerFinish').datepicker({
+    //     locale: 'fr',
+    //     useCurrent: true,
+    // });
+
+    $('#finishDate').bootstrapMaterialDatePicker({weekStart: 0, format : 'DD/MM/YYYY HH:mm'});
+    $('#startDate').bootstrapMaterialDatePicker({weekStart: 0, format : 'DD/MM/YYYY HH:mm'}).on('change', function(e, date) {
+      $('#finishDate').bootstrapMaterialDatePicker('setMinDate', date);
+    });
+
+    $('#datetimepickerStart button').click(function (e) {
+      console.log('variable');
+      $('#startDate').trigger("focus");
+    });
+    $('#datetimepickerFinish button').click(function (e) {
+      $('#finishDate').trigger("focus");
+    });
+  }
+  // initialisation de la carte Google Map de départ
+  function initMap() {
+    geocoder = new google.maps.Geocoder();
+    // Ici j'ai mis la latitude et longitude du vieux Port de Marseille pour centrer la carte de départ
+    var latlng = new google.maps.LatLng(48.856614, 2.3522219000000177);
+    var mapOptions = {
+      zoom      : 14,
+      center    : latlng,
+      mapTypeId : google.maps.MapTypeId.ROADMAP
+    }
+    // map-canvas est le conteneur HTML de la carte Google Map
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  }
+   
+  function findPlace() {
+    // Réinitialisation du marqueur   
+    if(marker != null)
+      marker.setMap(null);
+    // Récupération de l'adresse tapée dans le formulaire
+    var adresse = document.getElementById('maps').value;
+    geocoder.geocode( { 'address': adresse}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        // Récupération des coordonnées GPS du lieu tapé dans le formulaire
+        var strposition = results[0].geometry.location+"";
+        strposition=strposition.replace('(', '');
+        strposition=strposition.replace(')', '');
+        // Affichage des coordonnées dans le <span>
+        document.getElementById('text_latlng').innerHTML='Coordonnées : '+strposition;
+        //Récupération des coordonnées dans le formulaire
+        document.getElementById('latitude').value=results[0].geometry.location.lat();
+        document.getElementById('longitude').value=results[0].geometry.location.lng();
+        // Création du marqueur du lieu (épingle)
+        marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Adresse introuvable: ' + status);
+      }
+    });
+  }
+
+  /**
+   * Add Google Map find place listener
+   */
+  $('.findPlaceBtn').click(function (e) {
+    findPlace();
+  });
+});
