b00cf5d451c49c5531f8c03dd4db35866537cffd
Hervé Tutuaku <herve.tutuaku@gmail.com>
2016-07-18 10:31:36 +0200

    [feature] create a json response api

--- src/fr/pinnackl/rest/EventRest.java
+++ src/fr/pinnackl/rest/EventRest.java
@@ -1,67 +1,35 @@
 package fr.pinnackl.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
 
 import fr.pinnackl.bdd.Events;
 import fr.pinnackl.beans.Event;
 
-@Path("/events")
 public class EventRest {
-	@GET
-	@Produces("application/json")
-	public Response getEvents() throws JSONException {
 
-		List<JSONObject> jsonObjects = new ArrayList<JSONObject>();
+	public String getEvents() {
 
 		Events eventsDB = new Events();
-		List<Event> events = eventsDB.getEvents();
+		List<Event> eventList = eventsDB.getEvents();
 
-		for (Event event : events) {
-			JSONObject jsonObject = new JSONObject();
-			jsonObject.put("id", event.getID());
-			jsonObject.put("name", event.getName());
-			jsonObject.put("startDate", event.getStartDate());
-			jsonObject.put("finishDate", event.getFinishDate());
+		Gson gson = new Gson();
 
-			jsonObjects.add(jsonObject);
-		}
+		String json = gson.toJson(eventList);
 
-		return Response.status(200).type(MediaType.APPLICATION_JSON + "; charset=UTF-8").entity(jsonObjects.toString())
-				.build();
+		return json;
 	}
 
-	@Path("{id}")
-	@GET
-	@Produces("application/json")
-	public Response getEvent(@PathParam("id") Integer id) throws JSONException {
-
-		JSONObject jsonObject = new JSONObject();
+	public String getEvent(Integer id) {
 
 		Events eventsDB = new Events();
-
 		Event event = eventsDB.getEventByID(id);
 
-		if (event != null) {
-			jsonObject.put("id", event.getID());
-			jsonObject.put("name", event.getName());
-			jsonObject.put("startDate", event.getStartDate());
-			jsonObject.put("finishDate", event.getFinishDate());
+		Gson gson = new Gson();
 
-			return Response.status(200).type(MediaType.APPLICATION_JSON + "; charset=UTF-8")
-					.entity(jsonObject.toString()).build();
-		}
+		String json = gson.toJson(event);
 
-		return Response.status(404).build();
+		return json;
 	}
 }
